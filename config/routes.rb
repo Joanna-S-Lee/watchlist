c8b132324b82d2a4b724bbd509d0531e8d7d960e
Joanna-S-Lee <annaoj91@gmail.com>
2021-08-05 00:08:48 +1000

    added search bar added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'media/index'
   devise_for :users
+  # resources :users, only: [:index. :show]
   # root 'home#page'
-  root to: "medias#index"
+  root to: "home#page"
   resources :medias, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
